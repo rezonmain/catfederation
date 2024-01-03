e81b60d06070185817a8613a902060817ae45a65
rezonmain <adiazgomez101@gmail.com>
2024-01-03 11:57:49 -0800

    feat(helpers): add route helpers

---
+++ helpers/route.helpers.ts
@@ -0,0 +1,35 @@
+import { headers } from "next/headers";
+import { nil } from "@/helpers/utils.helpers";
+import { redirect } from "next/navigation";
+
+const getServerActionPathname = () => {
+  const heads = headers();
+  const url = heads.get("Referer");
+  if (nil(url)) {
+    throw new Error("No referer header found");
+  }
+  const urlObj = new URL(url);
+  return urlObj.pathname;
+};
+
+/**
+ * Redirect to the current page with the given search params
+ * @param params
+ */
+const returnWithSearchParams = <
+  T extends
+    | string
+    | string[][]
+    | Record<string, string>
+    | URLSearchParams
+    | undefined
+>(
+  params: T
+) => {
+  const redirectUrl = `${getServerActionPathname()}?${new URLSearchParams(
+    params
+  ).toString()}`;
+  redirect(redirectUrl);
+};
+
+export { getServerActionPathname, returnWithSearchParams };
