483739bc83bdedd481e1773635cc3985b2d7d87e
rezonmain <adiazgomez101@gmail.com>
2024-01-03 18:15:09 -0800

    feat(routeConstants): add user route

--- constants/route.constants.ts
+++ constants/route.constants.ts
@@ -1,5 +1,6 @@
 const ROUTE_SIGNUP_CONFIRM = "/signup/confirm";
 const ROUTE_SIGNUP = "/signup";
 const ROUTE_LOGIN = "/login";
+const ROUTE_USER = "/user";
 
-export { ROUTE_SIGNUP_CONFIRM, ROUTE_SIGNUP, ROUTE_LOGIN };
+export { ROUTE_SIGNUP_CONFIRM, ROUTE_SIGNUP, ROUTE_LOGIN, ROUTE_USER };
