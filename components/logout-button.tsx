924e1874ea01eeabd4707d0cce66a172e8187cc3
rezonmain <adiazgomez101@gmail.com>
2024-01-03 18:14:53 -0800

    feat(ui): add logout button

---
+++ components/logout-button.tsx
@@ -0,0 +1,14 @@
+import { SubmitButton } from "./submit-button";
+
+type LogoutButtonProps = {
+  action: () => void;
+};
+const LogoutButton: React.FC<LogoutButtonProps> = ({ action }) => {
+  return (
+    <form action={action}>
+      <SubmitButton>Logout</SubmitButton>
+    </form>
+  );
+};
+
+export { LogoutButton };
