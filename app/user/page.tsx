96b9296b43d62e0a71e0f012797d6e79c05ae445
rezonmain <adiazgomez101@gmail.com>
2024-01-04 23:35:09 -0800

    feat(userPage): update ui

--- app/user/page.tsx
+++ app/user/page.tsx
@@ -1,15 +1,12 @@
-import { handleLogout } from "actions";
-import { LogoutButton } from "@/components/logout-button";
 import { auth } from "@/helpers/session.helpers";
 
 export default function UserPage() {
   const { userId } = auth();
 
   return (
-    <main className="flex min-h-screen flex-col items-center gap-4 p-24">
+    <main className="flex flex-col items-center gap-4 p-24">
       <h1>cat federation</h1>
       <p>{userId}</p>
-      <LogoutButton action={handleLogout} />
     </main>
   );
 }
