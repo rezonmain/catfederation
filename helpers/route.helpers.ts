70e8960fc409c12bc6bff3a1f9da09216a6d44ff
rezonmain <adiazgomez101@gmail.com>
2024-01-06 11:39:47 -0800

    feat(routeHelpers): add fill dynamic path helper

--- helpers/route.helpers.ts
+++ helpers/route.helpers.ts
@@ -32,4 +32,28 @@ const returnWithSearchParams = <
   redirect(redirectUrl);
 };
 
-export { getServerActionPathname, returnWithSearchParams };
+// Borrowed from the internet
+type ExtractRouteParams<T> = string extends T
+  ? Record<string, string>
+  : T extends `${infer _Start}:${infer Param}/${infer Rest}`
+  ? { [k in Param | keyof ExtractRouteParams<Rest>]: string }
+  : T extends `${infer _Start}:${infer Param}`
+  ? { [k in Param]: string }
+  : object;
+
+const fillDynamicPath = <S extends string>(
+  path: S,
+  args: ExtractRouteParams<S>
+) => {
+  const parts = path.split("/");
+  parts.forEach((p, i) => {
+    if (p.startsWith(":")) {
+      const key = p.slice(1);
+      // @ts-expect-error key is a valid key of args
+      parts[i] = args[key];
+    }
+  });
+  return parts.join("/");
+};
+
+export { getServerActionPathname, returnWithSearchParams, fillDynamicPath };
