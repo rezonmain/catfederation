56a75587d5b5f83230a4ec745fdb1b2589dc79d6
rezonmain <adiazgomez101@gmail.com>
2024-01-05 16:15:03 -0800

    feat(developer): add developer page, route, layout

--- constants/route.constants.ts
+++ constants/route.constants.ts
@@ -2,5 +2,12 @@ const ROUTE_SIGNUP_CONFIRM = "/signup/confirm";
 const ROUTE_SIGNUP = "/signup";
 const ROUTE_LOGIN = "/login";
 const ROUTE_USER = "/user";
+const ROUTE_DEVELOPER = "/developer";
 
-export { ROUTE_SIGNUP_CONFIRM, ROUTE_SIGNUP, ROUTE_LOGIN, ROUTE_USER };
+export {
+  ROUTE_SIGNUP_CONFIRM,
+  ROUTE_SIGNUP,
+  ROUTE_LOGIN,
+  ROUTE_USER,
+  ROUTE_DEVELOPER,
+};
