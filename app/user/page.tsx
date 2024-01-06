c6c72243e41e7ccff21728465c8377cac90a4486
rezonmain <adiazgomez101@gmail.com>
2024-01-05 16:14:02 -0800

    feat(userPage): add link to developer page

--- app/user/page.tsx
+++ app/user/page.tsx
@@ -1,3 +1,5 @@
+import { Link } from "@/components/link";
+import { ROUTE_DEVELOPER } from "@/constants/route.constants";
 import { auth } from "@/helpers/session.helpers";
 
 export default function UserPage() {
@@ -7,6 +9,7 @@ export default function UserPage() {
     <main className="flex flex-col items-center gap-4 p-24">
       <h1>cat federation</h1>
       <p>{userId}</p>
+      <Link href={ROUTE_DEVELOPER}>Go to developer dashboard</Link>
     </main>
   );
 }
