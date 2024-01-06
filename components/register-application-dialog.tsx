e2295747974ef529069300b689e7c39c5fa9948e
rezonmain <adiazgomez101@gmail.com>
2024-01-05 16:15:41 -0800

    feat(ui): add register application dialog

---
+++ components/register-application-dialog.tsx
@@ -0,0 +1,64 @@
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Input } from "./ui/input";
+import { SubmitButton } from "./submit-button";
+import { Button } from "./ui/button";
+
+type RegisterApplicationDialogProps = {
+  children: React.ReactNode;
+  action: (formData: FormData) => Promise<unknown>;
+};
+
+const RegisterApplicationDialog: React.FC<RegisterApplicationDialogProps> = ({
+  children,
+  action,
+}) => {
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button>{children}</Button>
+      </DialogTrigger>
+      <DialogContent className="max-w-sm">
+        <DialogHeader>
+          <DialogTitle className="uppercase tracking-wider">
+            Register Application
+          </DialogTitle>
+          <DialogDescription>
+            Register an application to use catfederation as an OAuth2 provider.
+          </DialogDescription>
+        </DialogHeader>
+        <form id="register-new-app-form" action={action}>
+          <label className="flex flex-col gap-2">
+            Name
+            <Input
+              id="name"
+              name="name"
+              type="text"
+              placeholder="My Application"
+              required
+              className="border border-gray-300 rounded-md"
+            />
+          </label>
+        </form>
+        <DialogFooter>
+          <DialogClose asChild>
+            <Button type="button" variant="secondary">
+              Cancel
+            </Button>
+          </DialogClose>
+          <SubmitButton form="register-new-app-form">Register</SubmitButton>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+export { RegisterApplicationDialog };
