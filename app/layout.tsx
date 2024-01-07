ee40f02138771ea5265e227b5873b41db4733645
rezonmain <adiazgomez101@gmail.com>
2024-01-07 00:03:53 -0800

    feat(layout): add toaster component to global layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Noto_Sans } from "next/font/google";
 import "./globals.css";
+import { Toaster } from "@/components/ui/sonner";
 
 const noto = Noto_Sans({ subsets: ["latin"] });
 
@@ -16,7 +17,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={noto.className}>{children}</body>
+      <body className={noto.className}>
+        {children}
+        <Toaster />
+      </body>
     </html>
   );
 }
