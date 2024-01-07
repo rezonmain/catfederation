29735ee322b5801e2bfd5786e7b148c8a347de33
rezonmain <adiazgomez101@gmail.com>
2024-01-07 00:24:31 -0800

    feat(applicationPage): render user applications

--- app/developer/page.tsx
+++ app/developer/page.tsx
@@ -1,17 +1,44 @@
+import { redirect } from "next/navigation";
+import { Link } from "@/components/link";
 import { RegisterApplicationDialog } from "@/components/register-application-dialog";
 import { auth } from "@/helpers/session.helpers";
+import { developer } from "@/helpers/preference.helpers";
+import {
+  ROUTE_DEVELOPER_APPLICATION,
+  ROUTE_USER,
+} from "@/constants/route.constants";
+import { getApplicationsByUserId } from "@/repositories/applications.repository";
+import { fillDynamicPath } from "@/helpers/route.helpers";
 import { handleRegisterApplication } from "./action";
 
 export default async function DeveloperPage() {
   const { userId } = auth();
+  const isDeveloper = developer();
+  if (!isDeveloper) {
+    redirect(ROUTE_USER);
+  }
+
+  const applications = await getApplicationsByUserId({ userId });
 
   return (
     <main className="flex flex-col items-center gap-4 p-24">
       <h1>Developer Page</h1>
-      <p>{userId}</p>
       <RegisterApplicationDialog action={handleRegisterApplication}>
         New Application
       </RegisterApplicationDialog>
+      <div>
+        {applications.map((application) => (
+          <div key={application.id}>
+            <Link
+              href={fillDynamicPath(ROUTE_DEVELOPER_APPLICATION, {
+                applicationId: application.id,
+              })}
+            >
+              {application.name}
+            </Link>
+          </div>
+        ))}
+      </div>
     </main>
   );
 }
