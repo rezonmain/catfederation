a752117f841d930a1f73960b5a3d928a97f4c5da
rezonmain <adiazgomez101@gmail.com>
2024-01-07 00:05:16 -0800

    feat(userPage): update user page with preference form

--- app/user/page.tsx
+++ app/user/page.tsx
@@ -1,15 +1,30 @@
 import { Link } from "@/components/link";
+import { UserPreferenceForm } from "@/components/user-preference-form";
 import { ROUTE_DEVELOPER } from "@/constants/route.constants";
+import { userPreferences } from "@/helpers/preference.helpers";
 import { auth } from "@/helpers/session.helpers";
+import { handleUserPreferences } from "./action";
 
 export default function UserPage() {
-  const { userId } = auth();
+  auth();
+  const preferences = userPreferences();
 
   return (
     <main className="flex flex-col items-center gap-4 p-24">
-      <h1>cat federation</h1>
-      <p>{userId}</p>
-      <Link href={ROUTE_DEVELOPER}>Go to developer dashboard</Link>
+      {preferences.alias ? (
+        <h1>Hi {preferences.alias}!</h1>
+      ) : (
+        <h1>Hi there!</h1>
+      )}
+      {preferences.isDeveloper && (
+        <Link href={ROUTE_DEVELOPER}>Go to developer dashboard</Link>
+      )}
+      <section className="max-w-md">
+        <UserPreferenceForm
+          action={handleUserPreferences}
+          defaultValues={preferences}
+        />
+      </section>
     </main>
   );
 }
