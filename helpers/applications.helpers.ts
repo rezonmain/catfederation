050896bc553f6ef28bb1ba4005eba2eb4464e1e2
rezonmain <adiazgomez101@gmail.com>
2024-01-17 20:27:09 -0800

    feat(applicationHelpers): add getApplicationPageActions helper

--- helpers/applications.helpers.ts
+++ helpers/applications.helpers.ts
@@ -1,5 +1,18 @@
-import { APPLICATION_ID_LENGTH } from "@/constants/applications.constants";
+import {
+  APPLICATION_ID_LENGTH,
+  APPLICATION_SECRET_LENGTH,
+} from "@/constants/applications.constants";
 import { init } from "@paralleldrive/cuid2";
+import { getRandomBytes } from "./crypto.helpers";
+import { getBoundedActions } from "./route.helpers";
+import {
+  handleCreateApplicationRedirect,
+  handleDeleteApplication,
+  handleEditApplicationDescription,
+  handleEditApplicationName,
+  handleUpdateApplicationSecret,
+} from "@/app/developer/application/[applicationId]/actions";
+import { type Application } from "@/db/schema";
 
 const generateApplicationId = () => {
   return init({
@@ -7,4 +20,37 @@ const generateApplicationId = () => {
   })();
 };
 
-export { generateApplicationId };
+const generateApplicationSecret = () => {
+  return getRandomBytes(APPLICATION_SECRET_LENGTH);
+};
+
+const getApplicationPageActions = (applicationId: Application["id"]) => {
+  const [
+    editName,
+    editDescription,
+    createRedirect,
+    deleteApplication,
+    updateSecret,
+  ] = getBoundedActions(
+    applicationId,
+    handleEditApplicationName,
+    handleEditApplicationDescription,
+    handleCreateApplicationRedirect,
+    handleDeleteApplication,
+    handleUpdateApplicationSecret
+  );
+
+  return {
+    editName,
+    editDescription,
+    createRedirect,
+    deleteApplication,
+    updateSecret,
+  };
+};
+
+export {
+  generateApplicationId,
+  generateApplicationSecret,
+  getApplicationPageActions,
+};
