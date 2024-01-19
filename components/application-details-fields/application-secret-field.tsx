5de6e252725e588173174b68ad369754362943d8
rezonmain <adiazgomez101@gmail.com>
2024-01-18 20:12:34 -0800

    feat(components): add application secret field

---
+++ components/application-details-fields/application-secret-field.tsx
@@ -0,0 +1,49 @@
+"use client";
+import { useFormState } from "react-dom";
+import { handleUpdateApplicationSecret } from "@/app/developer/application/[applicationId]/actions";
+import { type ServerAction } from "@/types/common.types";
+import { ResetApplicationSecretDialog } from "@/components/reset-application-secret-dialog";
+import { empty } from "@/helpers/utils.helpers";
+import { ClipboardButton } from "../clipboard-button";
+
+type ActionReturnType = ReturnType<typeof handleUpdateApplicationSecret>;
+
+type ApplicationResetFieldProps = {
+  action: ServerAction<ActionReturnType>;
+};
+
+const ApplicationResetField: React.FC<ApplicationResetFieldProps> = ({
+  action,
+}) => {
+  const [secret, formAction] = useFormState<ActionReturnType>(action, "");
+
+  return (
+    <div className="rounded-lg border p-4">
+      <div className="flex flex-col gap-4">
+        <p className="uppercase tracking-wider">Application secret</p>
+        {empty(secret) ? null : (
+          <div className="flex flex-col gap-4">
+            <p className="text-small text-muted-foreground">
+              Save it somewhere safe, we will not show it again!
+            </p>
+            <label className="flex flex-row justify-between border items-center p-2 rounded-lg cursor-pointer">
+              <code className="text-sm">{secret}</code>
+              <ClipboardButton
+                text={secret}
+                toastMessage="Application secret copied to clipboard!"
+              />
+            </label>
+          </div>
+        )}
+        <ResetApplicationSecretDialog
+          action={formAction}
+          className="self-start"
+        >
+          Reset secret
+        </ResetApplicationSecretDialog>
+      </div>
+    </div>
+  );
+};
+
+export { ApplicationResetField };
