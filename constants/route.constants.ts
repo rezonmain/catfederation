549c7e7bbde90c9f5e71a9114f83033d6825cb10
rezonmain <adiazgomez101@gmail.com>
2024-01-26 20:55:28 -0800

    feat(routeConstants): add hyperspace

--- constants/route.constants.ts
+++ constants/route.constants.ts
@@ -4,6 +4,7 @@ const ROUTE_LOGIN = "/login";
 const ROUTE_USER = "/user";
 const ROUTE_DEVELOPER = "/developer";
 const ROUTE_DEVELOPER_APPLICATION = "/developer/application/:applicationId";
+const ROUTE_HYPERSPACE = "/hyperspace";
 
 export {
   ROUTE_SIGNUP_CONFIRM,
@@ -12,4 +13,5 @@ export {
   ROUTE_USER,
   ROUTE_DEVELOPER,
   ROUTE_DEVELOPER_APPLICATION,
+  ROUTE_HYPERSPACE,
 };
