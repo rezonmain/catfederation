76885e5c3a59ffd9462c8e4701f955ecf7111b36
rezonmain <adiazgomez101@gmail.com>
2024-01-26 21:05:00 -0800

    feat(hyperspace): redirect to home if callback is empty

--- components/hyperspace.tsx
+++ components/hyperspace.tsx
@@ -2,7 +2,7 @@
 import { useEffect } from "react";
 import { redirect } from "next/navigation";
 import { LoadingSpinner } from "@/components/ui/loading-spinner";
-import { ROUTE_USER } from "@/constants/route.constants";
+import { ROUTE_HOME } from "@/constants/route.constants";
 import { empty } from "@/helpers/utils.helpers";
 
 type HyperspaceProps = {
@@ -11,7 +11,7 @@ type HyperspaceProps = {
 const Hyperspace: React.FC<HyperspaceProps> = ({ callback }) => {
   useEffect(() => {
     if (empty(callback)) {
-      redirect(ROUTE_USER);
+      redirect(ROUTE_HOME);
     }
 
     redirect(callback);
