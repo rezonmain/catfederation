919609338ccef4dd7b6bb2ad08265b70386ba618
rezonmain <adiazgomez101@gmail.com>
2024-01-27 19:16:18 -0800

    feat(loginConstants): remove url validation for redirectTo

--- constants/login.constants.ts
+++ constants/login.constants.ts
@@ -5,10 +5,7 @@ const LOGIN_SCHEMA = z.object({
   password: z
     .string()
     .min(8, { message: "Password must be at least 8 characters long" }),
-  redirectTo: z
-    .string()
-    .url({ message: "Please provide a valid URL" })
-    .optional(),
+  redirectTo: z.string().nullable().optional(),
 });
 
 export { LOGIN_SCHEMA };
