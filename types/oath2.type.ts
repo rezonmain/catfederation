1f94c8765a2cec65c099c779db59112f2ec0c888
rezonmain <adiazgomez101@gmail.com>
2024-01-27 19:07:30 -0800

    feat(oauth2Types): remove redirect type

--- types/oath2.type.ts
+++ types/oath2.type.ts
@@ -2,9 +2,5 @@ import { OAUTH2_AUTHORIZE_SCHEMA } from "@/constants/oauth2.constants";
 import { type z } from "zod";
 
 type OAuth2AuthorizeParams = z.infer<typeof OAUTH2_AUTHORIZE_SCHEMA>;
-type OAuth2RedirectSearchParams = {
-  applicationId: string;
-  redirectParams: string;
-};
 
-export type { OAuth2AuthorizeParams, OAuth2RedirectSearchParams };
+export type { OAuth2AuthorizeParams };
