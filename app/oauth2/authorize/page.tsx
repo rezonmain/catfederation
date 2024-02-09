9fd7ce4787a0b12f9b4aadf04b0aadc3f2bcb718
rezonmain <adiazgomez101@gmail.com>
2024-02-08 17:38:07 -0800

    feat(oauth): add basic authorize page

---
+++ app/oauth2/authorize/page.tsx
@@ -0,0 +1,40 @@
+import { Link } from "@/components/link";
+import { getApplicationById } from "@/repositories/applications.repository";
+import { OAuth2AuthorizeParams } from "@/types/oath2.type";
+
+type OAuth2AuthorizePageSearchParams = OAuth2AuthorizeParams;
+
+export default async function OAuth2AuthorizePage({
+  searchParams,
+}: {
+  searchParams: OAuth2AuthorizePageSearchParams;
+}) {
+  const { applicationId, redirectUri } = searchParams;
+  const application = await getApplicationById({ applicationId });
+  return (
+    <div>
+      <small>The external application</small>
+      <h1>{application.name}</h1>
+      <small>Wants to access your catfederation account</small>
+      <hr />
+      <small>
+        By pressing the authorize button you will grant access to{" "}
+        {application.name} the following:
+      </small>
+      <ul>
+        <li>Your username and user ID</li>
+      </ul>
+      <hr />
+      <p>
+        <small>
+          Once you authorize the application you will be redirected to{" "}
+          <code>{redirectUri}</code>
+        </small>
+      </p>
+      <Link href={redirectUri}>Cancel</Link>
+      <form>
+        <button>Authorize</button>
+      </form>
+    </div>
+  );
+}
